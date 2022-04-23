7f0d3023e5891ed1c55ddc35ab581e9a9046f495
Tom Ainsworth <tomainsworth94@gmail.com>
2022-04-23 15:22:15 +0100

    Fix file path for questions.json

--- healthyplanet/static/js/quiz.js
+++ healthyplanet/static/js/quiz.js
@@ -1,4 +1,13 @@
+// $.getJSON("../static/js/questions.json", function (data) {
+//   console.log("It worked");
+// });
 
-    $.getJSON('../static/js/questions.json', function(data) {
-        console.log("It worked");
-    });
+$.ajax({
+    url: "../static/js/questions.json",
+    dataType: "json",
+    type: "GET",
+    cache: false,
+    success: function (data) {
+        console.log(data);
+    }
+})
