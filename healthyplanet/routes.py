3b1f87a2147cb1db1af7e29862a08115e26da614
Deborah Thompson <deb246@btinternet.com>
2022-04-24 17:29:18 +0100

    pledges form created

--- healthyplanet/routes.py
+++ healthyplanet/routes.py
@@ -1,5 +1,7 @@
 """ Routes """
-from flask import render_template
+
+from flask import render_template, request, url_for, redirect
+
 from healthyplanet import app
 
 
@@ -21,11 +23,27 @@ def quiz():
     return render_template("quiz.html")
 
 
-@app.route("/pledges")
+@app.route("/pledges", methods =["GET","POST"])
 def pledges():
-    """ Basic route """
-    return render_template("pledges.html")
+    """pledges logic"""
+    pledgesresult=[]
+    if request.method == "POST":
+        #print(request.form.getlist('pledges'))
+        pledgesresult = request.form.getlist('GET','pledges')
+        #print(pledgesresult)
+    #open file object and write to pledgesmade
+    #file.
+        with open('data/pledgesmade.txt','w') as file:
+            file.write(str(pledgesresult))
+            #print(pledgesresult)
+        return redirect(url_for ('yourpledges', pledgesresult=pledgesresult))
 
+    return render_template('pledges.html', pledgesresult=pledgesresult)
+
+#displays pledges made to user
+@app.route('/yourpledges/',methods=["GET", "POST"])
+def yourpledges():
+  return render_template('yourpledges.html')
 
 @app.route("/environmental-stories")
 def environmental_stories():
