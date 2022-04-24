18ef65b6f808b2e4fbdf1e9035b3de09c1b80fc2
Tom Ainsworth <tomainsworth94@gmail.com>
2022-04-24 02:33:47 +0100

    Fix quiz so it displays results page

--- healthyplanet/static/js/quiz.js
+++ healthyplanet/static/js/quiz.js
@@ -102,12 +102,12 @@ $(function () {
 			"answer": "The recycle truck may not pick it up."
 		}
 	]
-	const lastQuestion = 2;
+	const lastQuestion = questions.length;
 	let runningQuestion = 0;
 	let score = 0;
 	$("#start-quiz-btn").click(function () {
 		console.log("clicked")
-		showNextQuestion();
+		displayQuestion();
 	});
 
 	$('#btn-submit').click(function (e) {
@@ -118,7 +118,7 @@ $(function () {
 	/**
 	 * Display the "quiz-page" html, show each question in order if there are ny left otherwise call the collectResultsAndDisplayHouse.
 	 */
-	function showNextQuestion() {
+	function displayQuestion() {
 		if (runningQuestion < lastQuestion) {
 			const questionArray = questions[runningQuestion]
 			$("#home-page").addClass("hidden");
@@ -158,8 +158,6 @@ $(function () {
 				<span data-hover="${questionArray.optionB}">${questionArray.optionB}</span>
 				</label>
 			`)
-			} else {
-				displayResults();
 			};
 		}
 	}
@@ -168,57 +166,61 @@ $(function () {
 	 * When the submit button is clicked call the relavent functions to progress the quiz
 	 */
 	function submitAnswer() {
-		checkAnswerResult();
-		runningQuestion += 1;
-		showNextQuestion();
-	}
+		if (runningQuestion < lastQuestion) {
+			checkAnswerResult();
+			runningQuestion += 1;
+			displayQuestion();
 
-	/**
-	 * Check which answer-option has been checked, and record the result. Otherwise display an alert if nothing is selected.
-	 */
-	function checkAnswerResult() {
-		const questionArray = questions[runningQuestion]
-		if (!$("input:checked").val()) {
-			alert('Oops! Please pick an option and try again!');
-		} else if ($("input:checked").val() == questionArray.answer) {
-			score += 1;
+		} else {
+			displayResults();
 		}
 	}
+		/**
+		 * Check which answer-option has been checked, and record the result. Otherwise display an alert if nothing is selected.
+		 */
+		function checkAnswerResult() {
+			const questionArray = questions[runningQuestion]
+			if (!$("input:checked").val()) {
+				alert('Oops! Please pick an option and try again!');
+			} else if ($("input:checked").val() == questionArray.answer) {
+				score += 1;
+			}
+		}
 
 
-	/**
-	 * Displays the final score to the user
-	 */
-	function displayResults() {
+		/**
+		 * Displays the final score to the user
+		 */
+		function displayResults() {
 
-		$("#quiz-page").addClass("hidden");
-		$("#home-page").addClass("hidden");
-		$("#results-page").removeClass("hidden");
-		$('#results-page').html(`
+			$("#quiz-page").addClass("hidden");
+			$("#home-page").addClass("hidden");
+			$("#results-page").removeClass("hidden");
+			$('#results-page').html(`
 			<h2>Congratulations on finishing the Quiz!</h2>
             <p id="final-score">You scored ${score} out of ${lastQuestion}</p>
 			<button id="return-to-menu-btn" class="btn btn-primary btn-block">Start Again</button>`)
-		$("#return-to-menu-btn").click(function () {
+			$("#return-to-menu-btn").click(function () {
+				startAgainFromMenu()
+			});
+		}
+
+
+		/**
+		 * Resets all scores, hides results-page and quiz-page, and returns to the main screen
+		 */
+		function startAgainFromMenu() {
+			score = 0;
+			runningQuestion = 0;
+
+			$("#quiz-page").addClass("hidden");
+			$("#home-page").removeClass("hidden");
+			$("#results-page").addClass("hidden");
+		}
+
+
+		// Event listener to restart the quiz if "start again" button is clicked
+		$("#start-again-btn").click(function () {
 			startAgainFromMenu()
 		});
-	}
-
-
-	/**
-	 * Resets all scores, hides results-page and quiz-page, and returns to the main screen
-	 */
-	function startAgainFromMenu() {
-		score = 0;
-		runningQuestion = 0;
-
-		$("#quiz-page").addClass("hidden");
-		$("#home-page").removeClass("hidden");
-		$("#results-page").addClass("hidden");
-	}
-
-
-	// Event listener to restart the quiz if "start again" button is clicked
-	$("#start-again-btn").click(function () {
-		startAgainFromMenu()
-	});
 });
